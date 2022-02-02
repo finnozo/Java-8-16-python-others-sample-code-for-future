fe18a1b01639457f529148ec70e8024a1258480b
Sushil Dangi <dangi.sushil5@gmail.com>
2022-02-02 12:39:16 +0530

    static block

---
+++ JavaBasic/src/main/java/com/sushildangi/StaticBlock.java
@@ -0,0 +1,10 @@
+package com.sushildangi;
+
+public class StaticBlock {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    static {
+        System.out.println("Static block test");
+    }
+}
