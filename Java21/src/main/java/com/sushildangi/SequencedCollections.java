5abf8d9fa227427bfc81238da9598321af88a9ad
Sushil Dangi <dangi.sushil5@gmail.com>
2024-05-31 05:55:09 +0530

    Sequenced Collections

---
+++ Java21/src/main/java/com/sushildangi/SequencedCollections.java
@@ -0,0 +1,23 @@
+package com.sushildangi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequencedCollections {
+    void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        // TODO: BEFORE JAVA 21
+        int firstElement = list.get(0);
+        // Fetch the last element
+        int lastElement = list.get(list.size() - 1);
+
+        System.out.println(STR."First \{firstElement}, Last \{lastElement}");
+
+        // TODO: In JAVA 21
+        System.out.println(STR."First \{list.getFirst()}, Last \{list.getLast()}");
+    }
+}
